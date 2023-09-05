61a293f36622b11464d8a344e7b087512185a201
kevin <onofrekevin@gmail.com>
2023-09-05 13:01:56 -0500

    Implementacion de validar que talla se selecciono

--- components/products/SizeSelector.tsx
+++ components/products/SizeSelector.tsx
@@ -5,18 +5,20 @@ import { Box, Button } from '@mui/material';
 interface Props{
     childrend?: React.ReactNode,
     selectedSize?: ISize,
-    sizes: ISize[];
+    sizes: ISize[],
+    onSelectedSize: (size: ISize) => void;
 }
 
-export const SizeSelector: FC<Props> = ({selectedSize, sizes}) => {
+export const SizeSelector: FC<Props> = ({selectedSize, sizes,onSelectedSize }) => {
   return (
     <Box>
         {
-            sizes.map(size=> (
-                <Button key={size} size='small'
+          sizes.map(size=> (
+              <Button key={size} size='small'
                 color={selectedSize === size ? 'primary' : 'info'}
+                onClick={ ()=> onSelectedSize(size) }
                 > {size} </Button>
-            ))
+          ))
         }
     </Box>
   )
