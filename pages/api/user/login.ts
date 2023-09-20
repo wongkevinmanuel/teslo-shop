e10ee76129d9fc4e460b23878203ae4e80e3ba9e
kevin <onofrekevin@gmail.com>
2023-09-20 08:16:08 -0500

    Crear rest login

---
+++ pages/api/user/login.ts
@@ -0,0 +1,55 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { db } from '../../../database';
+import User from '../../../models/User';
+import bcrypt from 'bcryptjs';
+
+type Data = {
+    message: string
+}
+|
+{
+    token: string, //jwt
+    user: {
+            email:string,
+            role:string,
+            name: string
+        }
+}
+export default function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
+    switch(req.method){
+        case 'POST':
+            return loginUser(req, res);
+        case 'GET':
+            return res.status(200).json({ message:'Bad request GET.'});
+        default:
+            return res.status(200).json({ message: 'Bad request.' });
+    }
+}
+
+async function loginUser(request: NextApiRequest, response: NextApiResponse<Data>) {
+    const {email = '', password = ''} = request.body;
+
+    await db.connect();
+    const user = await User.findOne({email});
+    await db.disconnect();
+
+    if(!user){
+        return response.status(200).json({ message: 'Correo o password no validos. - EMAIL' });
+    }
+    
+    //comparar los hashs
+    //user.password! = siempre se tiene password en bd
+    //Si no son comparables los hash
+    if( ! bcrypt.compareSync(password, user.password!)){
+        return response.status(400).json({ message: 'Correo o password no validos. - Password' });
+    }
+    
+    const { role, name } = user;
+
+    return response.status(200).json({
+        token: '', //jwt
+        user: {
+            email, role, name
+        }
+    })
+}
