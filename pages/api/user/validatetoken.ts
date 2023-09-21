401fbd0e775c29cc61a0855c05587f6c90cf2351
kevin <onofrekevin@gmail.com>
2023-09-21 09:51:42 -0500

    Implementacion del validar y renovar JWT

---
+++ pages/api/user/validatetoken.ts
@@ -0,0 +1,58 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { db } from '../../../database';
+import User from '../../../models/User';
+import bcrypt from 'bcryptjs';
+import { jwt } from '../../../util';
+import { isValidToken } from '../../../util/jwt';
+
+type Data = {
+    message: string
+}
+|
+{
+    token: string,
+    user: {
+            email:string,
+            role:string,
+            name: string
+        }
+}
+
+export default function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
+    switch(req.method){
+        case 'GET':
+            return checkJWT(req, res);
+        default:
+            return res.status(200).json({ message: 'Bad request.' });
+    }
+}
+
+async function checkJWT(request: NextApiRequest, response: NextApiResponse<Data>) {
+    const { token = '' } = request.cookies;
+
+    let userId = '';
+   
+    try{
+        userId = await jwt.isValidToken(token);
+    }catch(error){
+        response.status(200).json({message:'Token de autorizacion no es valido'});
+    }
+    
+    await db.connect();
+    const userr = await User.findById(userId).lean();
+    await db.disconnect();
+
+    if(!userr){
+        return response.status(400)
+        .json({ message: 'Usuario no existe con ese id.' });
+    }
+     
+    return response.status(200).json({
+        token: jwt.signToken(userr._id, userr.email),
+        user: {
+            email: userr.email,
+            role: userr.role, 
+            name: userr.name
+        }
+    })
+}
--- util/jwt.ts
+++ util/jwt.ts
@@ -17,3 +17,30 @@ export const signToken = (_id: string, email:string): string => {
 
     return jwtToken;
 }
+
+export const isValidToken = ( token:string ):Promise<string> => {
+    if(!process.env.JWT_SECRET_SEED){
+        throw new Error(
+            'No hay semilla de JWT - Revisar variables de entorno'
+        );
+    }
+
+    const promesa: Promise<string> = new Promise( (resolve, reject) =>{
+        try{
+            jwt.verify(token, process.env.JWT_SECRET_SEED || '', 
+            (err, payload) => {
+                if(err) return reject('JWT no es valido');
+
+                const { _id } = payload as { _id:string };
+
+                resolve(_id);
+            })
+        }catch(error){
+            reject('JWT no es valido');
+        }
+    })
+    
+    return promesa;
+}
+
+
