12d61c0a10465b9675c2289248359f8dd0acdb17
kevin <onofrekevin@gmail.com>
2023-10-13 20:24:35 -0500

    exponer contexto de sesión, SessionProvider, en el nivel superior del app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,22 +7,26 @@ import { SWRConfig } from 'swr';
 import { AuthProvider, UiProvider } from '../context';
 import CartProvider from '../context/cart/CartProvider';
 
+import { SessionProvider } from "next-auth/react"
+
 function MyApp({Component, pageProps}:AppProps ){
     return (
-    <SWRConfig
-        value={{
-        fetcher: (resource, init) => fetch(resource, init).then(res => res.json())}}>
-        <AuthProvider>
-            <CartProvider>
-                <UiProvider>
-                    <ThemeProvider theme={lightTheme}>
-                        <CssBaseline />
-                        <Component {...pageProps} />
-                    </ThemeProvider>
-                </UiProvider>
-            </CartProvider>
-        </AuthProvider>
-    </SWRConfig>
+    <SessionProvider>    
+        <SWRConfig
+            value={{
+            fetcher: (resource, init) => fetch(resource, init).then(res => res.json())}}>
+            <AuthProvider>
+                <CartProvider>
+                    <UiProvider>
+                        <ThemeProvider theme={lightTheme}>
+                            <CssBaseline />
+                            <Component {...pageProps} />
+                        </ThemeProvider>
+                    </UiProvider>
+                </CartProvider>
+            </AuthProvider>
+        </SWRConfig>
+    </SessionProvider>
     )
 }
 
