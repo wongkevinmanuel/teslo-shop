1a5b9649ec1c8fbf17b3ee0afaf9f4295993848c
kevin <onofrekevin@gmail.com>
2023-11-20 22:01:46 -0500

    Página principal del Dashboard Administrativo

--- components/ui/SideMenu.tsx
+++ components/ui/SideMenu.tsx
@@ -3,7 +3,7 @@ import { AuthContext, UiContext } from "../../context"
 import { useRouter } from 'next/router'
 
 import { Box, Divider, Drawer, IconButton, Input, InputAdornment, List, ListItem, ListItemIcon, ListItemText, ListSubheader } from "@mui/material"
-import { AccountCircleOutlined, AdminPanelSettings, CategoryOutlined, ConfirmationNumberOutlined, EscalatorWarningOutlined, FemaleOutlined, LoginOutlined, MaleOutlined, SearchOutlined, VpnKeyOutlined } from "@mui/icons-material"
+import { AccountCircleOutlined, AdminPanelSettings, CategoryOutlined, ConfirmationNumberOutlined, DashboardOutlined, EscalatorWarningOutlined, FemaleOutlined, LoginOutlined, MaleOutlined, SearchOutlined, VpnKeyOutlined } from "@mui/icons-material"
 
 export const SideMenu = () => {
     
@@ -139,7 +139,7 @@ export const SideMenu = () => {
                             button
                             onClick={ ()=> navigateTo('/admin/') }>
                                 <ListItemIcon>
-                                    <CategoryOutlined/>
+                                    <DashboardOutlined/>
                                 </ListItemIcon>
                                 <ListItemText primary={'Dashboard'} />
                             </ListItem>
--- pages/admin/index.tsx
+++ pages/admin/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { AdminLayout } from '../../components/layouts'
-import { DashboardOutlined } from '@mui/icons-material'
+import { CreditCardOffOutlined, DashboardOutlined } from '@mui/icons-material'
+import { Card, CardContent, Grid, Typography } from '@mui/material'
 
 const DashboardPage = () => {
   return (
@@ -8,6 +9,57 @@ const DashboardPage = () => {
     title= 'Dashboard'
     subTitle= 'Estadisticas generales'
     icon={ <DashboardOutlined/> }>
+      <Grid container spacing={2}>
+        <Grid item xs={12} sm={4} md={3}>
+          <Card sx={{display:'flex'}}>
+            <CardContent sx={{width:50, display:'flex', justifyContent:'center',alignItems:'center'}}>
+              <CreditCardOffOutlined color="secondary" sx={{fontSize: 40}}>
+              </CreditCardOffOutlined>
+            </CardContent>
+            <CardContent sx={{flex:'1 0 auto', display:'flex', flexDirection:'column' }}>
+              <Typography variant='h3'> 50 </Typography>
+              <Typography variant='caption'> Ordenes Totales </Typography>
+            </CardContent>
+          </Card>
+        </Grid>
+        <Grid item xs={12} sm={4} md={3}>
+          <Card sx={{display:'flex'}}>
+            <CardContent sx={{width:50, display:'flex', justifyContent:'center',alignItems:'center'}}>
+              <CreditCardOffOutlined color="secondary" sx={{fontSize: 40}}>
+              </CreditCardOffOutlined>
+            </CardContent>
+            <CardContent sx={{flex:'1 0 auto', display:'flex', flexDirection:'column' }}>
+              <Typography variant='h3'> 50 </Typography>
+              <Typography variant='caption'> Ordenes Totales </Typography>
+            </CardContent>
+          </Card>
+        </Grid>
+        <Grid item xs={12} sm={4} md={3}>
+          <Card sx={{display:'flex'}}>
+            <CardContent sx={{width:50, display:'flex', justifyContent:'center',alignItems:'center'}}>
+              <CreditCardOffOutlined color="secondary" sx={{fontSize: 40}}>
+              </CreditCardOffOutlined>
+            </CardContent>
+            <CardContent sx={{flex:'1 0 auto', display:'flex', flexDirection:'column' }}>
+              <Typography variant='h3'> 50 </Typography>
+              <Typography variant='caption'> Ordenes Totales </Typography>
+            </CardContent>
+          </Card>
+        </Grid>
+        <Grid item xs={12} sm={4} md={3}>
+          <Card sx={{display:'flex'}}>
+            <CardContent sx={{width:50, display:'flex', justifyContent:'center',alignItems:'center'}}>
+              <CreditCardOffOutlined color="secondary" sx={{fontSize: 40}}>
+              </CreditCardOffOutlined>
+            </CardContent>
+            <CardContent sx={{flex:'1 0 auto', display:'flex', flexDirection:'column' }}>
+              <Typography variant='h3'> 50 </Typography>
+              <Typography variant='caption'> Ordenes Totales </Typography>
+            </CardContent>
+          </Card>
+        </Grid>
+      </Grid>
+
     </AdminLayout>
   )
 }
