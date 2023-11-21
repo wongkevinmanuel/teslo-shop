8497fc45c03d991d6f0bfc0c7cf287df27d98bd2
kevin <onofrekevin@gmail.com>
2023-11-21 09:31:44 -0500

    Componente Tarjeta para mostrar Dashboard Admin

--- components/admin/index.ts
+++ components/admin/index.ts
@@ -1,2 +1,2 @@
+export { SummaryTile } from './SummaryTile';
 export { AdminNavbar } from "./AdminNavbar";
- 
--- pages/admin/index.tsx
+++ pages/admin/index.tsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import { AdminLayout } from '../../components/layouts'
-import { CreditCardOffOutlined, DashboardOutlined } from '@mui/icons-material'
+import { AccessTimeOutlined, AttachMoneyOutlined, CancelPresentationOutlined, CategoryOutlined, CreditCardOffOutlined, CreditCardOutlined, DashboardOutlined, GroupAddOutlined, ProductionQuantityLimits } from '@mui/icons-material'
 import { Card, CardContent, Grid, Typography } from '@mui/material'
+import { SummaryTile } from '../../components/admin'
 
 const DashboardPage = () => {
   return (
@@ -10,54 +11,71 @@ const DashboardPage = () => {
     subTitle= 'Estadisticas generales'
     icon={ <DashboardOutlined/> }>
       <Grid container spacing={2}>
-        <Grid item xs={12} sm={4} md={3}>
-          <Card sx={{display:'flex'}}>
-            <CardContent sx={{width:50, display:'flex', justifyContent:'center',alignItems:'center'}}>
-              <CreditCardOffOutlined color="secondary" sx={{fontSize: 40}}>
-              </CreditCardOffOutlined>
-            </CardContent>
-            <CardContent sx={{flex:'1 0 auto', display:'flex', flexDirection:'column' }}>
-              <Typography variant='h3'> 50 </Typography>
-              <Typography variant='caption'> Ordenes Totales </Typography>
-            </CardContent>
-          </Card>
-        </Grid>
-        <Grid item xs={12} sm={4} md={3}>
-          <Card sx={{display:'flex'}}>
-            <CardContent sx={{width:50, display:'flex', justifyContent:'center',alignItems:'center'}}>
-              <CreditCardOffOutlined color="secondary" sx={{fontSize: 40}}>
-              </CreditCardOffOutlined>
-            </CardContent>
-            <CardContent sx={{flex:'1 0 auto', display:'flex', flexDirection:'column' }}>
-              <Typography variant='h3'> 50 </Typography>
-              <Typography variant='caption'> Ordenes Totales </Typography>
-            </CardContent>
-          </Card>
-        </Grid>
-        <Grid item xs={12} sm={4} md={3}>
-          <Card sx={{display:'flex'}}>
-            <CardContent sx={{width:50, display:'flex', justifyContent:'center',alignItems:'center'}}>
-              <CreditCardOffOutlined color="secondary" sx={{fontSize: 40}}>
-              </CreditCardOffOutlined>
-            </CardContent>
-            <CardContent sx={{flex:'1 0 auto', display:'flex', flexDirection:'column' }}>
-              <Typography variant='h3'> 50 </Typography>
-              <Typography variant='caption'> Ordenes Totales </Typography>
-            </CardContent>
-          </Card>
-        </Grid>
-        <Grid item xs={12} sm={4} md={3}>
-          <Card sx={{display:'flex'}}>
-            <CardContent sx={{width:50, display:'flex', justifyContent:'center',alignItems:'center'}}>
-              <CreditCardOffOutlined color="secondary" sx={{fontSize: 40}}>
-              </CreditCardOffOutlined>
-            </CardContent>
-            <CardContent sx={{flex:'1 0 auto', display:'flex', flexDirection:'column' }}>
-              <Typography variant='h3'> 50 </Typography>
-              <Typography variant='caption'> Ordenes Totales </Typography>
-            </CardContent>
-          </Card>
-        </Grid>
+        
+        <SummaryTile 
+            title={50} 
+            subTitle='Ordenes totales' 
+            icon={ <CreditCardOutlined
+                    color='secondary' sx={{fontSize: 40}}
+                    />
+                  } 
+          />
+        <SummaryTile 
+            title={50} 
+            subTitle='Ordenes pagadas' 
+            icon={ <AttachMoneyOutlined
+                    color='success' sx={{fontSize: 40}}
+                    />
+                  } 
+          />
+        <SummaryTile 
+            title={0} 
+            subTitle='Ordenes pendientes' 
+            icon={ <CreditCardOffOutlined
+                    color='error' sx={{fontSize: 40}}
+                    />
+                  } 
+          />
+        <SummaryTile 
+            title={0} 
+            subTitle='Clientes' 
+            icon={ <GroupAddOutlined
+                    color='success' sx={{fontSize: 40}}
+                    />
+                  } 
+          />
+        <SummaryTile 
+            title={0} 
+            subTitle='Productos' 
+            icon={ <CategoryOutlined
+                    color='secondary' sx={{fontSize: 40}}
+                    />
+                  } 
+          />
+        <SummaryTile 
+            title={0} 
+            subTitle='Productos sin Existencias' 
+            icon={ <CancelPresentationOutlined
+                    color='secondary' sx={{fontSize: 40}}
+                    />
+                  } 
+          />
+        <SummaryTile 
+            title={0} 
+            subTitle='Bajo inventario' 
+            icon={ <ProductionQuantityLimits
+                    color='warning' sx={{fontSize: 40}}
+                    />
+                  } 
+          />
+        <SummaryTile 
+            title={0} 
+            subTitle='Actualizacion en:' 
+            icon={ <AccessTimeOutlined
+                    color='secondary' sx={{fontSize: 40}}
+                    />
+                  } 
+          />
       </Grid>
 
     </AdminLayout>
