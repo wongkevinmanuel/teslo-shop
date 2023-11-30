7375c1130688dee034b5ba0aefd9cffcb696c663
kevin <onofrekevin@gmail.com>
2023-11-29 21:36:19 -0500

    Establecer propiedad renderCell en DataGrid

--- pages/admin/users.tsx
+++ pages/admin/users.tsx
@@ -3,138 +3,50 @@ import useSWR from 'swr';
 
 import { DataGrid, GridColDef, GridValueGetterParams,GridRowsProp, GridEventListener, GridRowId, GridCellModesModel, GridCellModes } from '@mui/x-data-grid';
 import { PeopleOutline } from '@mui/icons-material'
-import { Box, Button, Grid } from '@mui/material'
+import { Box, Button, Grid, MenuItem, Select } from '@mui/material'
 
 import { AdminLayout } from '../../components/layouts'
 import { IUser } from '../../interfaces';
-
-//Parámetros de celda seleccionados
-interface SelectedCellParams{
-    id: GridRowId;
-    field: string;
-}
-
-interface EditToolbarProps{
-    selectCellParams?: SelectedCellParams;
-    //se utiliza para controlar el modo de la
-    // celda seleccionada usando los botones externos
-    cellModesModel: GridCellModesModel;
-    setCellModesModel: (value: GridCellModesModel) => void;
-    cellMode: 'Ver' | 'Editar';
-}
-
-function EditToolbar(props: EditToolbarProps){
-    const { selectCellParams, cellMode, cellModesModel, setCellModesModel } = props;
-
-    const handleSaveOrEdit = () =>{
-        if(!selectCellParams){
-            return;
-        }
-
-        const { id, field } = selectCellParams;
-        
-        if(cellMode === 'Editar'){
-            setCellModesModel({
-                ...cellModesModel,
-                [id]: {...cellModesModel[id], [field]: {mode:GridCellModes.View} },
-            });
-        }else{
-            setCellModesModel({
-                ...cellModesModel,
-                [id]: { ...cellModesModel[id], [field]: {mode: GridCellModes.Edit} }
-            });
-        }
-    }
-    
-    const handleCancel = () => {
-        if(!selectCellParams){
-            return;
-        }
-        
-        const { id, field } = selectCellParams;
-
-        setCellModesModel({
-            ...cellModesModel,
-            [id]: {
-                ...cellModesModel[id],
-                [field]: { mode: GridCellModes.View, ignoreModifications: true}
-            }
-        });
-    }
-
-    const handleMouseDown = ( event: React.MouseEvent) => {
-        event.preventDefault();
-    }
-
-    return(
-        <Box 
-            sx={{ 
-                borderBottom: 1,
-                borderColor: 'divider',
-                p: 1
-            }}>
-                <Button
-                    onClick={handleSaveOrEdit}
-                    onMouseDown = {handleMouseDown}
-                    disabled={!selectCellParams}>
-                    {cellMode === 'Editar' ? 'Guardar': 'Editar' }
-                </Button>
-                <Button 
-                    onClick={handleCancel}
-                    onMouseDown = {handleMouseDown}
-                    disabled={cellMode === 'Ver'}
-                    variant="outlined"
-                    sx={{ ml:1 }}>
-                        
-                    Cancelar
-                </Button>
-        </Box>);
-}
+import tesloApi from '../../api';
 
 const usersPage = () => {
-        
-    /* MANEJO DEL DATAGRID */
-    const [selectedCellParams
-        , setSelectedCellParams] = useState< SelectedCellParams | null >(null);
-        
-    const [cellModesModel
-        , setCellModesModel] = useState<GridCellModesModel>({});
-            
+       
     const {data, error} = useSWR<IUser[]>('/api/admin/users');
     
     const [ users , setUsers] = useState<IUser[]>([]);
-    useEffect(()=> {
+    useEffect(()=> { // dispara establecer data en agreglo de usuarios
         if(data){
             setUsers(data);
         }
-    },[data])
+    },[data]) //agreglo de dependencia
+    
     // Si no hay data y no hay error
     //loading
-    //if(!data && !error ){ 
-    //    return (<></>)
-    //}
+    if(!data && !error ){ 
+        return (<></>)
+    }
 
-    // MANEJO DEL DATAGRID 
-    //manejo celda enfocada
-    const handleCellFocus = useCallback(
-        (event:React.FocusEvent<HTMLDivElement >) => {
-            const row = event.currentTarget.parentElement;
-            const id = row!.dataset.id!;
-            const field = event.currentTarget.dataset.field!;
+    const onRoleUpdate = async(userId: string, newRole:string )=> {
 
-            setSelectedCellParams({id, field });
-    }, [],);
-    
-    const cellMode = React.useMemo( () => {
-        if(!selectedCellParams){
-            return 'Ver';
+        const previousUsers = users.map( user => ({...user }) );
+        const updatedUsers = users.map( user => ({
+            ...user,
+            role: userId === user._id ? newRole : user.role
+        }));
+
+        setUsers(updatedUsers);
+
+        try{
+
+            await tesloApi.put('/admin/users', { userId, role: newRole});
+
+        }catch(error){
+            setUsers( previousUsers );
+            console.log(error);
+            alert('No se pudo actualizar el role del usuario');
         }
-    }, [cellModesModel, selectedCellParams ]);
 
-    const handleCellEditStop = React.useCallback<GridEventListener<'cellEditStop'> >(
-       (params, event )=> {
-            event.defaultMuiPrevented = true;
-       }, [] , );
+    }
 
     const columns: GridColDef [] = [
             { field: 'email'
@@ -143,17 +55,27 @@ const usersPage = () => {
             { field: 'name' 
             , headerName: 'Nombres', width: 300
              },
-            { field: 'role' 
-            , headerName: 'Rol', width: 300
+            { 
+            field: 'role' 
+            , headerName: 'Rol'
+            , width: 300
             , editable: true 
-            
-         }
+            , renderCell: ({row}: GridValueGetterParams) => {
+                return (<Select
+                    value={row.role}
+                    label="Rol"
+                    onChange={ ({target}) => onRoleUpdate( row.id, target.value) }
+                    sx={{width:'300px'}}
+                    >
+                        <MenuItem value='admin'> Admin </MenuItem>
+                        <MenuItem value='client'> Client </MenuItem>
+                        <MenuItem value='super-user'> Super User </MenuItem>
+                        <MenuItem value='SEO'> SEO </MenuItem>
+                    </Select>)
+                },
+            }
     ];
     
-    //{ field: 'role' , headerName: 'Rol', width: 300 , editable: true , type: 'singleSelect' 
-    // , valueOptions: ['admin','client'] }
-    //console.log(data);
-    //    const rows:GridRowsProp = data!.map( user => ({
     const rows:GridRowsProp = users!.map( user => ({
         id: user._id,
         email: user.email,
@@ -165,7 +87,7 @@ const usersPage = () => {
     <AdminLayout
         title={'Usuarios'}
         subTitle={'Mantenimiento de usuarios'}
-        icon={<PeopleOutline></PeopleOutline>}>
+        icon={ <PeopleOutline />}>
 
         <Grid container className='fadeIn'>
             <Grid item xs={12} sx={{ height: 650, width: '100%'}}>
@@ -173,30 +95,7 @@ const usersPage = () => {
                     
                     rows={ rows }
                     columns={ columns }
-                    //
-                    //getRowId={row => row._id}
-                    // Manejo DataGrid
-                    cellModesModel={ cellModesModel}
-                    onCellEditStop={ handleCellEditStop }
-                    onCellModesModelChange={ (model) => setCellModesModel(model)}
                     
-                    //Editar rol
-                    slots={{
-                        toolbar: EditToolbar
-                    }}
-
-                    slotProps={{
-                        toolbar: {
-                            cellMode,
-                            selectedCellParams,
-                            setSelectedCellParams,
-                            cellModesModel,
-                            setCellModesModel
-                        }, cell: {
-                            onFocus: handleCellFocus
-                        }
-                    }}
-
                     />
             </Grid>
         </Grid>
