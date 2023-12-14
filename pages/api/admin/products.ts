09942f797a2ced725200be98b48ec54c137bd25d
kevin <onofrekevin@gmail.com>
2023-12-13 20:54:07 -0500

    Metodo actualizar producto en API Administrador

--- pages/api/admin/products.ts
+++ pages/api/admin/products.ts
@@ -2,6 +2,7 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 import { IProduct } from '../../../interfaces'
 import { db } from '../../../database';
 import { Product } from '../../../models';
+import { isValidObjectId } from 'mongoose';
 
 type Data = 
 |{  message: string }
@@ -87,7 +88,55 @@ async function productFindOne(slug:string ){
     }
 
 
-function updateProduct(req: NextApiRequest, res: NextApiResponse<Data>) {
-    throw new Error('Function not implemented.');
+async function updateProduct(req: NextApiRequest, res: NextApiResponse<Data>) {
+    const {_id ='', images= [] } = req.body as IProduct;
+
+    if(!isValidObjectId(_id)){
+        return res.status(400).json({message:'El id del producto no es válido'});
+    }
+    
+    if(images.length < 2 ){
+        return res.status(400).json({message:'Es necesario al menos 2 imágenes'});
+    }
+
+    //TODO: posiblemente tendremos un localhost:3000/products/ass.jpg
+    try{
+
+        const product = await findByIdProduct( _id );
+        if(!product){
+            db.disconnect();
+            return res.status(400).json({ message: 'No existe un producto con ese ID'});
+        }
+
+        // TODO: eliminar fotos en Cloudinary
+        // https://res.cloudinary.com/cursos-udemy/image/upload/v1645914028/nct31gbly4kde6cncc6i.jpg
+        product.images.forEach( async (image) => {
+            if(!images.includes(image)){
+                //Borrar cloudinary
+                const [fileId, extension ] = image.substring(image.lastIndexOf('/')+1).split('.');
+                console.log({image, fileId, extension});
+                //await cloudinary.uploader.destroy(fileId);
+            }
+        });
+
+        await product.update(req.body);
+        await db.disconnect();
+
+        return res.status(200).json(product);
+
+    }catch(error){
+        console.log(error);
+        await db.disconnect();
+        return res.status(400).json({message: 'Revisar logs del servidor'});
+    }
 }
 
+async function findByIdProduct( _id:string ){
+    try{
+        await db.connect();
+        return await Product.findById(_id);
+    }catch(err){
+        await db.disconnect();
+        return false;
+    }
+}
